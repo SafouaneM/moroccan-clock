913ff81a6b13dedd899be88c488ace8aa26c7d4b
Safouane <31611670+SafouaneM@users.noreply.github.com>
2024-10-31 11:53:34 +0100

    wip 2

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ const minuteHand = document.querySelector('.minute-hand');
 const secondHand = document.querySelector('.second-hand');
 
 function setDate() {
-    const now = new Date().getTimezoneOffset();
+    const now = new Date();
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
@@ -87,10 +87,9 @@ function setDate() {
     minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + 90;
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
-
 setDate();
